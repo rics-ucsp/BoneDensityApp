3f54e7f3db3d81a923ff301d3637a27c98783be1
Bryan Gonzales Vega <bgonzales.vega@gmail.com>
2017-08-14 22:02:02 -0500

    Insert segmentation layotu

---
+++ StackedSegmentMesh.h
@@ -0,0 +1,22 @@
+#ifndef STACKEDSEGMENTMESH_H
+#define STACKEDSEGMENTMESH_H
+
+#include <QWidget>
+
+namespace Ui {
+class StackedSegmentMesh;
+}
+
+class StackedSegmentMesh : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit StackedSegmentMesh(QWidget *parent = 0);
+    ~StackedSegmentMesh();
+
+private:
+    Ui::StackedSegmentMesh *ui;
+};
+
+#endif // STACKEDSEGMENTMESH_H
