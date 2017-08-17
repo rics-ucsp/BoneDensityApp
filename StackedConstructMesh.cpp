eaf90db13b72b54cfccb8dfdc8f5f1e17437ed6c
tlchoque <tlchoque@gmail.com>
2017-08-17 09:35:35 -0500

    mm

--- StackedConstructMesh.cpp
+++ StackedConstructMesh.cpp
@@ -17,9 +17,11 @@ StackedConstructMesh::~StackedConstructMesh()
 
 void StackedConstructMesh::constructMesh()
 {
-    QVTKWidgetRenderer::renderVTK("../Resources/knees/construction_edgeCoordinates.vtk", *(ui->stacked_construction_edge), 0.05, (float [3]){1.0, 1.0, 1.0}, (float [3]){0.0, 0.0, 0.0});
+	float p1[] = { 1.0, 1.0, 1.0 }; float p2[] = { 0.0, 0.0, 0.0 };
+    QVTKWidgetRenderer::renderVTK("../Resources/knees/construction_edgeCoordinates.vtk", *(ui->stacked_construction_edge), 0.05, p1, p2);
     QVTKWidgetRenderer::renderColoredVTK("../Resources/knees/construction_edgeDensity.vtk", *(ui->stacked_construction_skeleton) );
-    QVTKWidgetRenderer::renderVTK("../Resources/knees/kmeshcm.vtk", *(ui->stacked_construction_kmeshcm), 2.0, (float [3]){0, 0, 0}, (float [3]){1.0, 1.0, 1.0});
-    QVTKWidgetRenderer::renderVTK("../Resources/knees/segment_knee_mesh_region_1.vtk", *(ui->stacked_construction_kmeshim), 1.0, (float [3]){0.9608, 0.8706, 0.7020}, (float [3]){0.0, 0.0, 0.0} );
+    QVTKWidgetRenderer::renderVTK("../Resources/knees/kmeshcm.vtk", *(ui->stacked_construction_kmeshcm), 2.0, p2, p1);
+	float p3[] = { 0.9608, 0.8706, 0.7020 }; float p4[] = { 0.9608, 0.8706, 0.7020 };
+    QVTKWidgetRenderer::renderVTK("../Resources/knees/segment_knee_mesh_region_1.vtk", *(ui->stacked_construction_kmeshim), 1.0, p3,p4 );
     
 }
